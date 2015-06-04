7826c07184a88c2a1203152a68fb03831a82bd24
gcanti <giulio.canti@gmail.com>
2015-06-04 07:42:12 +0200

    change Query API from positional to named arguments
    #14

--- src/Query.js
+++ src/Query.js
@@ -1,33 +1,10 @@
 import t from 'tcomb';
 
-export default class Query {
+const Query = t.struct({
+  id: t.Str,
+  get: t.Func,
+  fetch: t.maybe(t.Func),
+  dependencies: t.maybe(t.list(t.Str))
+});
 
-  constructor(id, get, fetch, dependencies) {
-    if (process.env.NODE_ENV !== 'production') {
-      const example = `
-
-Example:
-
-new Query(
-  'querySomething',
-  () => this.db.getSomething(),
-  () => { // optional
-    return this.API.fetchSomething().then(
-      something => this.update(db => db.setSomething(new Something(something)))
-    );
-  },
-  ['dependency1', 'dependency2'] // optional
-);
-      `;
-      t.assert(t.Str.is(id), `a Query must have a id: t.Str field. ${example}`);
-      t.assert(t.Func.is(get), `a Query must have a get: t.Func field. ${example}`);
-      t.assert(t.maybe(t.Func).is(fetch), `a Query may have a fetch: t.maybe(t.Func) field. ${example}`);
-      t.assert(t.maybe(t.list(t.Str)).is(dependencies), `a Query may have a dependencies: t.maybe(t.list(t.Str)) field. ${example}`);
-    }
-    this.id = id;
-    this.get = get;
-    this.fetch = fetch;
-    this.dependencies = dependencies;
-  }
-
-}
+export default Query;
