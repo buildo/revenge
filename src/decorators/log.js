232f09156f168d54029ba8032069dd9592681842
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-16 12:40:46 +0200

    make eslint happy

--- src/decorators/log.js
+++ src/decorators/log.js
@@ -5,14 +5,14 @@ export default function log(namespace) {
 
   if (process.env.NODE_ENV === 'production') {
     return function (target, name, descriptor) {
-      return target[name] = f;
+      return target[name] = descriptor.value; //eslint-disable-line no-return-assign
     };
   }
 
   const logger = debug(namespace || 'revenge:@log');
   return function (target, name, descriptor) {
     const f = descriptor.value;
-    return target[name] = function (...args) {
+    return target[name] = function (...args) { //eslint-disable-line no-return-assign
       const ret = f.apply(this, args);
       const message = `${name}(${args.map(stringify).join(', ')}) => ${stringify(ret)}`;
       logger(message);
