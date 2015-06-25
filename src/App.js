13ad2c5d98db0881e4a880f983535925934297bc
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-25 16:37:21 +0200

    restore update() + some commentary

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Avenger, { QuerySetInput } from 'avenger';
 export default class App {
 
   constructor(queries = {}, cacheInitialState = {}) {
+    // TODO(gio): app itself is an emitter.. not needed
+    // except for manual updates in db using update()
     this.emitter = new EventEmitter3();
     this.avenger = new Avenger(queries, cacheInitialState);
+  }
+
+  update(f) {
+    if (process.env.NODE_ENV !== 'production') {
+      t.assert(t.Func.is(f), `${this.constructor.name}.update(f) expects a function`);
+    }
+    this.emitter.emit('stateWillChange');
+    f();
+    this.emitter.emit('stateDidChange');
   }
 
   fetch(routes, params, query): Promise {
@@ -22,6 +33,8 @@ export default class App {
       this.qs.off('change');
     }
 
+    // TODO(gio): assuming an unique query set per
+    // per instance simultaneously
     this.qs = this.avenger.querySet({
       queries,
       state: {
@@ -31,13 +44,16 @@ export default class App {
 
     this.qs.on('change', data => {
       this._get = data;
-      this.emitter.emit('change');
+      update(() => {});
     });
 
     return qs.run();
   }
 
   get() {
+    // hack: work around the fact that
+    // @listener and @queries are two distinct entities
+    // listener update event handler already has access to this data
     return this._get || {};
   }
 
