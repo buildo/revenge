1abb897823cfc306bc6098c876170707bde84de5
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-26 18:08:07 +0200

    @queries: export cleaned up queries on original Component

--- src/decorators/queries.js
+++ src/decorators/queries.js
@@ -61,6 +61,7 @@ export default function queries(declared) {
       });
     }, {});
     d(`${Component.name} cleaned up queries: %o`, QueriesWrapper.queries);
+    Component.queries = QueriesWrapper.queries;
 
     return QueriesWrapper;
   };
