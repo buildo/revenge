606294bbbcdb5c29e69ad6a911ab0a57349c5831
Francesco Giordano <francesco.giordano@buildo.io>
2016-05-11 11:32:13 +0200

    default getLocals should just return props

--- src/decorators/skinnable.js
+++ src/decorators/skinnable.js
@@ -1,6 +1,10 @@
 import t from 'tcomb';
 import isReactComponent from '../isReactComponent';
 
+const defaultGetLocals = function(props) {
+  return props;
+};
+
 export default function skinnable(template?: Function): Function {
 
   return function (Component) {
@@ -10,7 +14,7 @@ export default function skinnable(template?: Function): Function {
       t.assert(t.maybe(t.Func).is(template), `@skinnable decorator can only be configured with a function`);
       t.assert(isReactComponent(Component), `@skinnable decorator can only be applied to React.Component(s). Maybe did you type @skinnable instead of @skinnable()?`);
       t.assert(!t.Func.is(Component.prototype.render), `@skinnable decorator can only be applied to components not implementing the render() method. Please remove the render method of component ${name}`);
-      t.assert(t.Func.is(Component.prototype.getLocals), `@skinnable decorator requires a getLocals() method, add it to component ${name}`);
+      t.assert(t.maybe(t.Func).is(Component.prototype.getLocals), `@skinnable decorator requires getLocals() to be a function, check the component ${name}`);
       if (template) {
         t.assert(!t.Func.is(Component.prototype.template), `@skinnable decorator can only be applied to components not implementing the template(locals) method. Please remove the template method of component ${name}`);
       }
@@ -21,6 +25,10 @@ export default function skinnable(template?: Function): Function {
 
     if (template) {
       Component.prototype.template = template;
+    }
+
+    if (!Component.prototype.getLocals) {
+      Component.prototype.getLocals = defaultGetLocals;
     }
 
     Component.prototype.render = function () {
