cf08c6ed5b62bf28282d6f620bacde33e35f8f87
Giovanni Gonzaga <giovanni@buildo.io>
2016-05-28 18:41:17 +0200

    Create contains.js

---
+++ src/contains.js
@@ -0,0 +1,4 @@
+// to be passed as template to @skinnable
+// e.g @skinnable(contains(Component))
+
+export default Component => locals => locals === null ? null : <Component {...locals} />; //eslint-disable-line react/display-name
