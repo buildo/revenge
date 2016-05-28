ba0189a3c0345b4eec64f9308adc525cc5f5a3de
Giovanni Gonzaga <giovanni@buildo.io>
2016-05-28 18:41:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export log from './decorators/log';
 export pure from './decorators/pure';
 export pureFunctions, { pureFunctionProp } from './decorators/pureFunctions';
 export skinnable from './decorators/skinnable';
+export contains from './contains';
 export mapProps from './decorators/mapProps';
