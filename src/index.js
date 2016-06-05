320c0945b3934cd2cfd7f3a7a45953b3a55448dd
Francesco <firefelix@gmail.com>
2016-06-05 16:12:32 +0200

    export pureLog from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export isReactComponent from './isReactComponent';
 export log from './decorators/log';
 export pure from './decorators/pure';
+export pureLog from './decorators/pureLog';
 export pureFunctions, { pureFunctionProp } from './decorators/pureFunctions';
 export skinnable from './decorators/skinnable';
 export contains from './contains';
